c2bef0f07c05fbdee7ddbb8116feece206174f61
Rafael Epplee <raffomania@gmail.com>
2019-07-26 21:46:30 +0200

    make rockets move

--- rocket.gd
+++ rocket.gd
@@ -6,12 +6,17 @@ var velocity
 func _ready():
 	target = $'../planet1'
 	rotation_degrees = 180
-	velocity = Vector2(3, 0).rotated(rotation)
+	velocity = Vector2(130, 0).rotated(rotation)
 
 func _draw():
-	draw_rect(Rect2(Vector2(0, 0), Vector2(15, 5)), Color(0, 0, 255))
+	draw_rect(Rect2(Vector2(0, 0), Vector2(10, 3)), Color(0, 0, 255))
 
 func _process(delta):
-	velocity += velocity.rotated(position.angle_to(target.position) * 0.01)
+	var target_angle = (target.position - position).angle()
+	var angle_diff = target_angle - velocity.angle()
+	velocity = velocity.rotated(angle_diff * 1 * delta)
+	var acceleration = max(0, 1 - angle_diff) * delta
+	velocity = velocity * (1 + acceleration)
+
 	position += velocity * delta
 	rotation = velocity.angle()
