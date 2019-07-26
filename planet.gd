7dd17b060702f9de1838ff66ed05ad58bf934158
trixxo <patrice.epplee@gmail.com>
2019-07-26 21:22:34 +0200

    income

--- planet.gd
+++ planet.gd
@@ -4,12 +4,16 @@ export var planetRadius = 100
 export (int) var playerNumber
 var player
 var angle = 0
+export var income = 0 
+var money = 0 
 
 func _ready():
 	player = preload("res://player.gd").new()
 	add_child(player)
 	player.position.y -= planetRadius
 	player.playerNumber = playerNumber
+	$Label.rect_position.x -= $Label.rect_size.x / 2
+	$Label.rect_position.y -= $Label.rect_size.y / 2
 
 func _draw():
 	draw_circle(Vector2(0, 0), planetRadius, Color(0, 255, 0))
@@ -17,4 +21,5 @@ func _draw():
 func _process(delta):
 	# player.position = player.position.rotated(0.5 * delta)
 	# player.rotation += 0.5 * delta
-	pass
+	money += income * delta
+	$Label.text = str(int(money))
