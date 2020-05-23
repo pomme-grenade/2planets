2061e3ee6e4c063a04926fe6dd075b5b30939a03
trixxo <patrice.epplee@gmail.com>
2020-05-23 23:51:25 +0200

    buttons and upgrades

--- sceneSwitcher.gd
+++ sceneSwitcher.gd
@@ -6,9 +6,14 @@ var _params = null
 # Call this instead to be able to provide arguments to the next scene
 func change_scene(next_scene, params=null):
 	_params = params
-	#warning-ignore:return_value_discarded
-	get_tree().change_scene(next_scene)
 
+	var peer = get_tree().network_peer
+	# get_tree().set_network_peer(peer)
+
+	get_tree().change_scene(next_scene)
+	var selfPeerID = get_tree().get_network_unique_id()
+	get_node('/root').set_network_master(selfPeerID)
+	
 # In the newly opened scene, you can get the parameters by name
 func get_param(name):
 	if _params != null and _params.has(name):
