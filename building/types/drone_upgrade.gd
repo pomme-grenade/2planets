32f58a12e2230216c46e4fd5921194539da56579
trixxo <patrice.epplee@gmail.com>
2020-06-10 15:56:38 +0200

    drone position

--- building/types/drone_upgrade.gd
+++ building/types/drone_upgrade.gd
@@ -20,7 +20,7 @@ func buildup_finish():
 func new_drone():
 	repair_drone = preload('res://repair_drone.tscn').instance()
 	repair_drone.position = repair_drone.position.rotated(position.direction_to(Vector2(0, 0)).angle() - PI/2)
-	repair_drone.position += get_parent().position + Vector2(0, -20)
+	repair_drone.position += get_parent().position + Vector2(0, 20).rotated(Vector2(0, 0).direction_to(get_parent().position).angle() - PI/2)
 	repair_drone.z_index = 3 if round(rand_range(1, 3)) == 1 else 1
 	planet.add_child(repair_drone)
 	repair_drone.init()
