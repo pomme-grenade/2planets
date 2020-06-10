afdaf03972f488136b03cccc7249e070f0d230d1
trixxo <patrice.epplee@gmail.com>
2020-06-10 18:30:43 +0200

    buildings dont work on build and destruction

--- building/types/attack.gd
+++ building/types/attack.gd
@@ -11,7 +11,7 @@ func try_fire_rocket(name):
 	if planet.money < 10 or (not is_network_master()):
 		planet.current_money_label.flash()
 		return
-	elif get_parent().is_built:
+	elif get_parent().is_built and not get_parent().is_destroyed:
 		var position = global_position - Vector2(5, 0).rotated(global_rotation)
 		rpc('fire_rocket', name, position, global_rotation + PI)
 
