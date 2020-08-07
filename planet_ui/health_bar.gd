0f220365b46f8ace9352703f2081b291a2a4daa0
trixxo <patrice.epplee@gmail.com>
2020-08-07 15:02:17 +0200

    add health bar

--- planet_ui/health_bar.gd
+++ planet_ui/health_bar.gd
@@ -1,53 +1,45 @@
+#warning-ignore-all:return_value_discarded
+
 tool
 extends Node2D
 
 var planet
 var health = 100
-var delayed_health = 100
-var background = 100
-var tween
-var timer_started = false
-var tween_timer
-var interpolation_started = false
-var expected_health_decrease
+var animated_health = health
+var background = health
+var tween: Tween
+var tween_target = health
+var tween_start_value = health
 
-const interpolation_duration = 0.5
+const tween_duration = 0.4
+const default_tween_delay_duration = 0.5 
+var tween_delay_duration = default_tween_delay_duration
 
 func _ready():
 	tween = $'Tween'
-	tween_timer = Timer.new()
-	tween_timer.connect('timeout', self, 'toggle_tween')
-	add_child(tween_timer)
 
 func _draw():
 	draw_line(Vector2(0, 0), Vector2(background * 0.62, 0), Color(0, 0, 0), 7)
-	draw_line(Vector2(0, 0), Vector2((delayed_health) * 0.62, 0), Color(1, 1, 1), 7)
+	draw_line(Vector2(0, 0), Vector2((animated_health) * 0.62, 0), Color(1, 1, 1), 7)
 	draw_line(Vector2(0, 0), Vector2(health * 0.62, 0), Color(0.6, 0.6, 0.9), 7)
 
-func _process(_delta):
-	if health < delayed_health and not timer_started:
-		tween_timer.start(0.5)
-		timer_started = true
-		expected_health_decrease = health
-	elif timer_started and health < expected_health_decrease:
-		tween_timer.stop()
-		tween_timer.start(0.5)
-		expected_health_decrease = health
-		if interpolation_started:
-			tween.interpolate_property(self, 'delayed_health',
-				delayed_health, health, interpolation_duration,
-				Tween.TRANS_QUINT, Tween.EASE_IN)
-
+func _process(_delta: float):
+	var delay_in_effect = tween.is_active() and round(animated_health) == round(tween_start_value)
+	var should_animate = health < tween_target
+	if should_animate and (not tween.is_active() or delay_in_effect):
+		start_tween()
+		tween_delay_duration = default_tween_delay_duration
+	elif should_animate and not delay_in_effect:
+		tween_delay_duration = 0
+		
 	update()
 
-func toggle_tween():
-	if !interpolation_started:
-		tween.interpolate_property(self, 'delayed_health',
-			delayed_health, health, interpolation_duration,
-			Tween.TRANS_QUINT, Tween.EASE_IN)
-		tween.start()
-		tween_timer.start(interpolation_duration)
-	elif interpolation_started:
-		timer_started = false
-		
-	interpolation_started = !interpolation_started
+func start_tween():
+	print('start tween')
+	tween.remove_all()
+	tween_start_value = animated_health
+	tween_target = health
+	tween.interpolate_property(self, 'animated_health',
+		animated_health, health, tween_duration,
+		Tween.TRANS_QUINT, Tween.EASE_IN, tween_delay_duration)
+	tween.start()
