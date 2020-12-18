2828ca6d6e52f8e90d27c7455e36439907cc58a9
trixxo <patrice.epplee@gmail.com>
2020-12-18 20:56:28 +0100

    fix

--- planet_ui/planet_ui.gd
+++ planet_ui/planet_ui.gd
@@ -152,8 +152,10 @@ func start_building(type: String):
 func start_upgrade(index):
 	if ((not is_instance_valid(player.current_building)
 		or player.current_building.is_destroyed)
-			or player.planet.money <= buildings.costs[get_upgrade_type(index)]):
+			or get_upgrade_type(index) == null
+				or player.planet.money <= buildings.costs[get_upgrade_type(index)]):
 		return
+		
 
 	if (not was_double_press('upgrade_building/upgrade_%d' % index,
 			get_upgrade_type(index))):
