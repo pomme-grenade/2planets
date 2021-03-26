e430b2475800d9437b3f4af4ea786076de990f1d
trixxo <patrice.epplee@gmail.com>
2021-03-26 13:47:39 +0100

    animated laser

--- laser/laser.gd
+++ laser/laser.gd
@@ -1,27 +1,44 @@
 extends Node2D
 var planet
 var enemy_player_number
-var target_planet
 var buildings
 var stop_laser_timer
 var shooting = false
+var can_activate = true
 var laser_position = 0
 var activate_cost = 20
 var laser_range = 300
 var one_building_destroyed = false
 var building_info
+var initial_delay = 0.5
+var initial_delay_timer 
+var animated_beam = AnimatedTexture.new()
+var beam_texture1
 
 func init():
 	building_info = ''
+
+	enemy_player_number = 1 if planet.player_number == 2 else 2
+
 	stop_laser_timer = Timer.new()
+	initial_delay_timer = Timer.new()
+
 	stop_laser_timer.connect('timeout', self, 'stop_laser')
 	add_child(stop_laser_timer)
-	enemy_player_number = 1 if planet.player_number == 2 else 2
-	target_planet = get_node('/root/main/planet_%s' % enemy_player_number)
+
+	initial_delay_timer.connect('timeout', self, 'start_shooting')
+	add_child(initial_delay_timer)
+
+	beam_texture1 = preload('res://laser/beam1.png')
+	var beam_texture2 = preload('res://laser/beam2.png')
+	animated_beam.set_frame_texture(0, beam_texture1)
+	animated_beam.set_frame_texture(1, beam_texture2)
+	animated_beam.set_frames(2)
+	animated_beam.set_fps(30)
 
 func _process(_dt):
-	buildings = get_tree().get_nodes_in_group('building' + str(enemy_player_number))
 	if shooting:
+		buildings = get_tree().get_nodes_in_group('building' + str(enemy_player_number))
 		for building in buildings:
 			var distance_to_building = Vector2(0, 0).distance_to(to_local(building.global_position))
 
@@ -35,18 +52,25 @@ func _process(_dt):
 
 func _draw():
 	if shooting:
-		draw_line(Vector2(0, 0), Vector2(0, -laser_position), Color(1, 1, 1), 1)
+		for n in range (1, laser_position, beam_texture1.get_size().y):
+			draw_texture(animated_beam, Vector2(-beam_texture1.get_size().y / 4, -n - beam_texture1.get_size().y))
 
 func stop_laser():
 	laser_position = 0
 	shooting = false
+	can_activate = true
 	one_building_destroyed = false
 	stop_laser_timer.stop()
 	update()
 
+func start_shooting():
+	shooting = true
+	initial_delay_timer.stop()
+	stop_laser_timer.start(0.27)
+	laser_position = laser_range
+	update()
+
 func on_activate():
-	if not shooting:
-		stop_laser_timer.start(0.07)
-		shooting = true
-		laser_position = laser_range
-		update()
+	if can_activate:
+		can_activate = false
+		initial_delay_timer.start(initial_delay)
