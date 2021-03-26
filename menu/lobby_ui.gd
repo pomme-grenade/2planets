06b8b1f0fa607932db4c8016705176a75980ef56
Rafael Epplee <raffomania@gmail.com>
2021-03-26 15:59:48 +0100

    implement cancel in lobby ui

--- menu/lobby_ui.gd
+++ menu/lobby_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var networking = get_node('lobby_networking')
+var waiting_for_network := false
 
 func _ready():
 	# warning-ignore:return_value_discarded
@@ -13,24 +14,49 @@ func _ready():
 	# warning-ignore:return_value_discarded
 	networking.connect('exit_lobby', self, '_on_exit_lobby')
 
-	$'network/game_code_label'.visible = false
+	reset_networking()
 
 func _on_local():
 	networking.server_for_local_game()
 
 func _on_connect():
-	$'network/create'.disabled = true
-	$'network/connect_container/connect'.disabled = true
+	if waiting_for_network:
+		# player pressed 'cancel'
+		reset_networking()
+		return
+
+	waiting_for_network = true
+	$'network/create'.visible = false
+	$'network/client_status'.visible = true
+	$'network/client_status'.text = 'Connecting to server...'
+	$'network/connect_container/connect'.text = 'cancel'
 
 	networking.connect_to_server()
 
 func _on_server():
+	if waiting_for_network:
+		# player pressed 'cancel'
+		reset_networking()
+		return
+
+	waiting_for_network = true
 	$'network/create'.text = 'Cancel'
 	$'network/connect_container'.visible = false
-	$'network/game_code_label'.visible = true
-	$'network/game_code_label'.text = 'Server started.\nWaiting for other player...'
+	$'network/server_status'.visible = true
+	$'network/server_status'.text = 'Server started.\nWaiting for other player...'
 
 	networking.start_server()
+
+func reset_networking():
+	$'network/server_status'.visible = false
+	$'network/client_status'.visible = false
+
+	$'network/create'.visible = true
+	$'network/create'.text = 'start server'
+	$'network/connect_container'.visible = true
+	$'network/connect_container/connect'.text = 'connect'
+
+	waiting_for_network = false
 
 # remove the lobby scene completely, unpause
 func _on_exit_lobby():
