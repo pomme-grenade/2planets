c0fd0240f647e647266459441b0c52841ec30cd4
Rafael Epplee <raffomania@gmail.com>
2021-04-12 11:20:51 +0200

    add sound toggle

---
+++ menu/toggle_sound.gd
@@ -0,0 +1,18 @@
+extends CheckBox
+var config
+
+
+func _ready():
+	config = ConfigFile.new()
+	var err = config.load('user://settings.cfg')
+	if err != OK and err != ERR_FILE_NOT_FOUND:
+		print('could not load settings file! ', err)
+	var sound_enabled = config.get_value('sound', 'enabled', true)
+	self.pressed = sound_enabled
+
+func _toggled(active: bool) -> void:
+	AudioServer.set_bus_mute(0, not active)
+	config.set_value('sound', 'enabled', active)
+	config.save('user://settings.cfg')
+
+
