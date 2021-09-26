ddae07c62f2b8959b3cd0442968369865d7ca90d
trixxo <patrice.epplee@gmail.com>
2021-09-26 19:43:11 +0200

    who removed the teleported??!

--- income/income.gd
+++ income/income.gd
@@ -5,7 +5,7 @@ var additional_income := default_additional_income
 # warning-ignore:unused_class_variable
 var upgrade_1_type := 'repair_drone_factory'
 # warning-ignore:unused_class_variable
-# var upgrade_2_type := 'teleporter'
+var upgrade_2_type := 'teleporter'
 var building_info
 var bonus_per_building := 0.1
 var connection_bonus := 0.0
